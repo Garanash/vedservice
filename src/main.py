6f06060b35806940421ace5b8e407d4fe5416ffe
a.dolgov <dolgov_am@mail.ru>
2025-04-21 17:02:32 +0300

    fixes

--- src/main.py
+++ src/main.py
@@ -2,14 +2,14 @@ import uvicorn
 from fastapi import FastAPI, Request
 from fastapi.templating import Jinja2Templates
 
-temlates = Jinja2Templates(directory='templates')
+templates = Jinja2Templates(directory='templates')
 
 application = FastAPI()
 
 
 @application.get('/')
 def start_page(request: Request):
-    return temlates.TemplateResponse('start.html', {'request': request})
+    return templates.TemplateResponse('index.html', {'request': request})
 
 
 if __name__ == '__main__':
